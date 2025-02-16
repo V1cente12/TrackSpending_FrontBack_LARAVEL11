2c6eb126f35dfea5b03f7891a11f88d9340f122a
V1cente12 <jvicentecv@gmail.com>
2025-02-15 21:05:09 -0400

    migrations

--- database/migrations/2025_02_16_233044_create_transactions_table.php
+++ database/migrations/2025_02_16_233044_create_transactions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('category_id')->constrained()->onDelete('set null');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('set null');
             $table->enum('type', ['income', 'expense']);
             $table->decimal('amount', 10, 2);
             $table->date('date');
