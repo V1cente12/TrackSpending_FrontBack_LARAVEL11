7bb86db1b5abe6f91ad5f12a03709109ebfbc413
V1cente12 <jvicentecv@gmail.com>
2025-02-22 15:21:37 -0400

    pwa

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
+        <title inertia>ViWallet</title>
+        <link rel="icon" type="image/x-icon" href="{{ asset('ViWallet.ico') }}">
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -14,6 +14,13 @@
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
+
+        <link rel="manifest" href="{{ asset('manifest.json') }}">
+        <script>
+            if ('serviceWorker' in navigator) {
+                navigator.serviceWorker.register("{{ asset('serviceworker.js') }}");
+            }
+        </script>
     </head>
     <body class="font-sans antialiased">
         @inertia
