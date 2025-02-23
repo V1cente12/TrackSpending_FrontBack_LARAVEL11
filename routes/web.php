a15228ab3e34a21e4466703b65a87bea82c848e5
V1cente12 <jvicentecv@gmail.com>
2025-02-23 00:12:39 -0400

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,22 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+use App\Http\Controllers\InitialBalanceController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CurrencyController;
 
 Route::get('/', function () {
     return redirect()->route('login');
-});
+})->middleware('guest');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/select-currency', [CurrencyController::class, 'show'])->name('select-currency');
+    Route::post('/set-currency', [CurrencyController::class, 'store'])->name('set-currency');
+    Route::get('/initial-balance', [InitialBalanceController::class, 'show'])->name('initial-balance');
+    Route::post('/set-initial-balance', [InitialBalanceController::class, 'store'])->name('set-initial-balance');
 });
