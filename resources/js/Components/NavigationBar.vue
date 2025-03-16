87482c050d93e8a1fb54130de6b9496c0a9f251a
V1cente12 <jvicentecv@gmail.com>
2025-03-16 00:01:14 -0400

    fix nav bar

--- resources/js/Components/NavigationBar.vue
+++ resources/js/Components/NavigationBar.vue
@@ -1,22 +1,34 @@
 <template>
-  <div class="bg-gray-800 text-white p-4 flex justify-around mb-4 mx-4 rounded-lg shadow-lg">
+  <div class="text-white p-4 flex justify-around border-t border-x border-gray-800 rounded-t-2xl bg-gray-800">
     <Link href="/dashboard" class="hover:text-gray-300">Home</Link>
     <button class="hover:text-gray-300">Profile</button>
-    <button class="hover:text-gray-300">Reports</button>
+    <button @click="showReports = true" class="hover:text-gray-300">Reports</button>
+    <button class="hover:text-gray-300">Categories</button>
   </div>
+
+  <ReportsModal 
+    :is-open="showReports"
+    @close="showReports = false"
+  />
 </template>
 
 <script>
+import { ref } from 'vue'
 import { Link } from '@inertiajs/vue3'
+import ReportsModal from './ReportsModal.vue'
 
 export default {
   name: 'NavigationBar',
   components: {
-    Link
+    Link,
+    ReportsModal
+  },
+  setup() {
+    const showReports = ref(false)
+    
+    return {
+      showReports
+    }
   }
 }
 </script>
-
-<style scoped>
-/* Estilos personalizados para la barra de navegación si lo deseas */
-</style>
