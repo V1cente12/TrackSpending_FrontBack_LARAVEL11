6f2f734579797a2d6ddf8ee3168158910b4dd9a5
V1cente12 <jvicentecv@gmail.com>
2025-03-15 23:59:17 -0400

    disable visibility on montly spending

--- resources/js/Components/MonthlySpendingCard.vue
+++ resources/js/Components/MonthlySpendingCard.vue
@@ -1,22 +1,55 @@
 <template>
-  <div class="bg-white p-3 rounded-lg shadow-lg">
-    <h3 class="text-xl font-semibold text-gray-700">Monthly</h3>
-    <p class="text-3xl font-bold text-red-600 mt-2">Bs {{ amount }}</p>
+  <div class="bg-gray-800 p-3 rounded-lg shadow-lg">
+    <div class="flex justify-between items-center">
+      <h3 class="text-xl font-semibold text-white">Monthly</h3>
+      <button @click="toggleVisibility" class="text-gray-600 hover:text-gray-200">
+        <i class="fas" :class="isHidden ? 'fa-eye-slash' : 'fa-eye'"></i>
+      </button>
+    </div>
+    <p class="text-[1.75rem] font-bold text-red-600 mt-2" :class="{ 'blur-sm': isHidden }">
+      {{ currencySymbol }} {{ amount }}
+    </p>
   </div>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue'
+
 export default {
   name: 'MonthlySpendingCard',
   props: {
     amount: {
       type: Number,
       required: true
+    },
+    currencySymbol: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    const isHidden = ref(false)
+    
+    onMounted(() => {
+      const savedState = localStorage.getItem('monthlyHidden')
+      isHidden.value = savedState === 'true'
+    })
+    
+    const toggleVisibility = () => {
+      isHidden.value = !isHidden.value
+      localStorage.setItem('monthlyHidden', isHidden.value)
+    }
+
+    return {
+      isHidden,
+      toggleVisibility
     }
   }
 }
 </script>
 
 <style scoped>
-/* Estilos específicos si lo necesitas */
+.blur-sm {
+  filter: blur(6px);
+}
 </style>
