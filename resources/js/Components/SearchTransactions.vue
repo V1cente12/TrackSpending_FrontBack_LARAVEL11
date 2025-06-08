ac1d2efc7f8e783a6f3db96ccd4c15039df25f19
V1cente12 <jvicentecv@gmail.com>
2025-06-07 22:36:13 -0400

    search engine improvement

--- resources/js/Components/SearchTransactions.vue
+++ resources/js/Components/SearchTransactions.vue
@@ -4,7 +4,7 @@
         type="text"
         v-model="searchQuery"
         placeholder="Find a transaction..."
-        class="w-full bg-gray-800 text-gray-100 rounded-xl px-4 py-2 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+        class="w-full bg-gray-800 text-gray-100 rounded-xl px-4 py-2 "
         @input="search"
       />
     </div>
@@ -24,12 +24,28 @@
   const searchQuery = ref('');
   
   const search = () => {
-    const query = searchQuery.value.toLowerCase();
-    const filtered = props.transactions.filter(transaction => 
-      transaction.description.toLowerCase().includes(query)
+  const query = searchQuery.value.toLowerCase();
+
+  const filtered = props.transactions.filter(transaction => {
+    const formattedDate = new Date(transaction.date).toLocaleDateString('es-BO', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }).toLowerCase();
+
+    return (
+      transaction.description.toLowerCase().includes(query) ||
+      transaction.type.toLowerCase().includes(query) ||
+      transaction.category?.name?.toLowerCase().includes(query) ||
+      transaction.payment_method?.name?.toLowerCase().includes(query) ||
+      transaction.amount.toString().includes(query) ||
+      formattedDate.includes(query) // Ahora se puede buscar: "2 de junio de 2025"
     );
-    emit('search', filtered);
-  };
+  });
+
+  emit('search', filtered);
+};
+
   
   watch(() => props.transactions, () => {
     searchQuery.value = '';
