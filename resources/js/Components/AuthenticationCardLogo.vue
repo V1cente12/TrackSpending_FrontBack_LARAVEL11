06482410b28edc6f8edbfb22081ccbd3a3b4feaf
V1cente12 <jvicentecv@gmail.com>
2026-09-08 16:25:33 -0400

    chore(auth): rebrand to consegsa and disable registration
    add new consegsa.png logo asset to public images
    disable Fortify registration feature via config comment
    update authentication card logo to new Consegsa branding
    remove unused registration link and associated script code

--- resources/js/Components/AuthenticationCardLogo.vue
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -3,5 +3,5 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <img src="/images/ViWallet.png" alt="ViWallet" class="h-16 w-auto" />
+    <img src="/images/consegsa.png" alt="Consegsa" class="h-16 w-auto" />
 </template>
